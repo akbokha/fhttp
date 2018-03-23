ab1124e998a80ebe2f689a11a1f45c8a6f7c5b1f
Adriaan Knapen <a.d.knapen@protonmail.com>
2018-03-23 16:47:08 +0100

    fix: incorrect value used in evaluating a test case

--- test/test_network_discovery.py
+++ test/test_network_discovery.py
@@ -14,10 +14,10 @@ class TestNetworkDiscovery(AbstractTest):
             self.assertIn(expected_ip, real_mapping)
             self.assertEqual(expected_mapping[expected_ip], real_mapping[expected_ip])
 
-    def test_get_own_mac_adress(self):
-        self.assertEqual('08:00:27:32:f4:6', NetworkDiscoverer().get_own_mac_address(True))
+    def test_get_own_mac_address(self):
+        self.assertEqual('08:00:27:32:f4:6a', NetworkDiscoverer().get_own_mac_address(True))
 
-    def test_get_own_ip_adress(self):
+    def test_get_own_ip_address(self):
         self.assertEqual('192.168.56.103', NetworkDiscoverer().get_own_ip_address(True))
 
 
