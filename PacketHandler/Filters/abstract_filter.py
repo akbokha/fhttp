dd6780ee6ae5b4012957c469e77d8da2d9de496e
Adriaan Knapen <a.d.knapen@protonmail.com>
2018-04-07 11:35:55 +0200

    new: added doc comments

--- PacketHandler/Filters/abstract_filter.py
+++ PacketHandler/Filters/abstract_filter.py
@@ -8,9 +8,18 @@ class AbstractFilter:
 
     def is_filtered(self, packet):
         # type: (Ether) -> bool
+        """
+        Decides if a packet is filtered or not
+        :param Ether packet:
+        :return bool: A boolean
+        """
         return self.to_string(packet) is not None
 
     @abstractmethod
     def to_string(self, packet):
         # type: (Ether) -> str or None
+        """
+        :param packet: The packet to be filtered.
+        :return: None when the packet is not filtered, or a string containing the relevant filtered part of the packet.
+        """
         pass
